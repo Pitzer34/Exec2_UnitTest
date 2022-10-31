d6cd9170b359e2d40afed28d45ab1ccae167223f
leo34 <leo34@DESKTOP-9UAA6AI>
2022-10-31 23:15:11 +0800

    加入專案檔案。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WindowsFormsApp1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lblmsg = new System.Windows.Forms.Label();
+			this.txtDate = new System.Windows.Forms.TextBox();
+			this.btnDateCheck = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// lblmsg
+			// 
+			this.lblmsg.AutoSize = true;
+			this.lblmsg.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.lblmsg.Location = new System.Drawing.Point(36, 62);
+			this.lblmsg.Name = "lblmsg";
+			this.lblmsg.Size = new System.Drawing.Size(115, 30);
+			this.lblmsg.TabIndex = 0;
+			this.lblmsg.Text = "查詢日期:";
+			// 
+			// txtDate
+			// 
+			this.txtDate.Location = new System.Drawing.Point(157, 70);
+			this.txtDate.Name = "txtDate";
+			this.txtDate.Size = new System.Drawing.Size(100, 22);
+			this.txtDate.TabIndex = 1;
+			// 
+			// btnDateCheck
+			// 
+			this.btnDateCheck.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.btnDateCheck.Location = new System.Drawing.Point(111, 121);
+			this.btnDateCheck.Name = "btnDateCheck";
+			this.btnDateCheck.Size = new System.Drawing.Size(75, 30);
+			this.btnDateCheck.TabIndex = 2;
+			this.btnDateCheck.Text = "查詢";
+			this.btnDateCheck.UseVisualStyleBackColor = true;
+			this.btnDateCheck.Click += new System.EventHandler(this.btnDateCheck_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(303, 194);
+			this.Controls.Add(this.btnDateCheck);
+			this.Controls.Add(this.txtDate);
+			this.Controls.Add(this.lblmsg);
+			this.Name = "Form1";
+			this.Text = "台灣股市營業日查詢";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label lblmsg;
+		private System.Windows.Forms.TextBox txtDate;
+		private System.Windows.Forms.Button btnDateCheck;
+	}
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnDateCheck_Click(object sender, EventArgs e)
+		{
+			DateTime dt;
+			
+			try
+			{
+				dt = DateRule();
+				string result = TradingTime(dt);
+				MessageBox.Show(result);
+			}
+			catch(Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+			
+		}
+
+		private DateTime DateRule()
+		{
+			string input = txtDate.Text;
+			bool isDateTime = DateTime.TryParse(input, out DateTime dt);
+
+			if (isDateTime == false)
+			{
+				throw new Exception("格式錯誤");
+			}
+			return dt;
+		}
+		private string TradingTime(DateTime dt)
+		{
+			string result;
+
+			int day = Convert.ToInt32(dt.DayOfWeek);
+			
+			DateTime time = Convert.ToDateTime(dt.ToString("HH:mm"));
+			DateTime StartTime = Convert.ToDateTime("09:00");
+			DateTime EndTime = Convert.ToDateTime("13:30");
+
+			if (day > 5 || day < 1)
+			{
+				result = "非營業日期";
+			}
+			else if (time > EndTime || time < StartTime)
+			{
+				result = "非營業時間";
+			}
+			else
+			{
+				result = dt.ToString("yyyy/MM/dd,HH:mm")+"為營業時間";
+			}
+			return result;
+		}
+	}
+}
